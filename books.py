a52d882d672f828f6b3b9d21192afbc10a55f5b1
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-18 19:05:56 +0600

    [ADDED] Search book by rating added

--- books.py
+++ books.py
@@ -41,19 +41,35 @@ class BookRequest(BaseModel):
 
 
 BOOKS = [
-    Book(1, 'CS 1101', 'Hasan', 'This is hasan great book', 4.5),
+    Book(1, 'CS 1101', 'Hasan', 'This is hasan great book', 5),
     Book(2, 'CS 1102', 'Hasan', 'This is hasan 2 great book', 4.75),
     Book(3, 'CS 1103', 'Hasan', 'This is hasan 3 great book', 4.25),
-    Book(4, 'CS 1104', 'Hasan', 'This is hasan 4 great book', 4.15),
+    Book(4, 'CS 1104', 'Hasan', 'This is hasan 4 great book', 5),
     Book(5, 'CS 1105', 'Hasan', 'This is hasan 5 great book', 4.00),
-    Book(6, 'CS 1106', 'Hasan', 'This is hasan 6 great book', 4.30),
-    Book(7, 'CS 1107', 'Hasan', 'This is hasan 6 great book', 3.8),
+    Book(6, 'CS 1106', 'Hasan', 'This is hasan 6 great book', 4.00),
+    Book(7, 'CS 1107', 'Hasan', 'This is hasan 6 great book', 4.00),
 ]
 
 
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: float):
+    return_all_books_with_rating=[]
+    for book in BOOKS:
+        if book.rating == book_rating:
+            return_all_books_with_rating.append(book)
+    return return_all_books_with_rating
 
 
 @app.post("/create-book")
