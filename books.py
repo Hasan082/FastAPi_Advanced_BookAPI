96e1dfaa90696e62f3ecb38959ff41415dfb928a
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-18 14:38:42 +0600

    [ADDED] Added Pydantic Model

--- books.py
+++ books.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI(title="Books Advanced API by Hasan", version='1.0.0')
 
@@ -18,6 +19,14 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: float
+
+
 BOOKS = [
     Book(1, 'CS 1101', 'Hasan', 'This is hasan great book', 4.5),
     Book(2, 'CS 1102', 'Hasan', 'This is hasan 2 great book', 4.75),
@@ -32,3 +41,9 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.dict())
+    BOOKS.append(new_book)
